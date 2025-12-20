5f577e066bca2f64bdb60dc0537e0a04a2c1c77d
Chase Manning <chase@manning.dev>
2025-12-20 08:55:47 +0000

    :rocket: add crimson blaze expansion

--- scripts/transpose-format.py
+++ scripts/transpose-format.py
@@ -27,6 +27,7 @@ set_mapping = {
     "Deluxe Pack: ex": "a4b",
     "Mega Rising  (B1)": "b1",
     "Promo-B": "pb",
+    "Crimson Blaze  (B1a)": "b1a",
 }
 
 series_map = {
@@ -44,6 +45,7 @@ series_map = {
     "a4b": {"endpoint": "A4b", "PacksNumber": 1},
     "b1": {"endpoint": "B1", "PacksNumber": 3},
     "pb": {"endpoint": "P-B", "PacksNumber": 3},
+    "b1a": {"endpoint": "B1a", "PacksNumber": 1},
 }
 
 
